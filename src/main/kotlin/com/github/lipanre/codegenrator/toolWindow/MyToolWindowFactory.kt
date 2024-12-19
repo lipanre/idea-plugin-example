2b68d49d0e86ce93dd72c1c6c4b86c4d7b845165
lipanre <lipanre@qq.com>
2024-12-19 11:10:10 +0800

    feat: 更新kotlin dsl示例

--- src/main/kotlin/com/github/lipanre/codegenrator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lipanre/codegenrator/toolWindow/MyToolWindowFactory.kt
@@ -10,6 +10,12 @@ import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.github.lipanre.codegenrator.MyBundle
 import com.github.lipanre.codegenrator.services.MyProjectService
+import com.intellij.openapi.observable.properties.ObservableMutableProperty
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.builder.text
 import javax.swing.JButton
 
 
@@ -31,15 +37,53 @@ class MyToolWindowFactory : ToolWindowFactory {
 
         private val service = toolWindow.project.service<MyProjectService>()
 
-        fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+        private val dataModal = DataModal(MyBundle.message("randomLabel", "?"))
 
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
-                addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
-                }
-            })
+        private lateinit var panel: DialogPanel
+
+        private val observer = object : ObservableMutableProperty<String> {
+
+
+            override fun set(value: String) {
+                dataModal.text = value
+            }
+
+            override fun get(): String {
+                return dataModal.text
+            }
+
         }
+
+//        private val textField: JBTextField = JBTextField()
+
+//        fun getContent() = JBPanel<JBPanel<*>>().apply {
+//            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+//
+//            add(label)
+//            add(JButton(MyBundle.message("shuffle")).apply {
+//                addActionListener {
+//                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+//                }
+//            })
+//        }
+
+        fun getContent() = JBPanel<JBPanel<*>>().apply {
+            panel = panel {
+                row {
+                    val textField = textField().bindText(observer)
+                    button(MyBundle.message("shuffle")) {
+                        thisLogger().warn("before data Modal text is: ${dataModal.text}")
+                        observer.set(MyBundle.message("randomLabel", service.getRandomNumber()))
+                        thisLogger().warn("after data Modal text is: ${dataModal.text}")
+//                        textField.text(dataModal.text)
+                        panel.apply()
+                    }
+                }
+            }
+            add(panel)
+        }
+
+
+        data class DataModal(var text: String)
     }
 }
